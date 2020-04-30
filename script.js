53fe5753f0ee3abec485400c10f3caf586e00272
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-30 14:18:10 -0700

    Made three API calls and returned objects.

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
 let cityName = "Chicago";
 let apiKey = "f61c25ccc3ebc66abfbc574449b8e000";
-let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 $.ajax({
-    url: weatherURL,
+    url: weatherUrl,
     method: "GET"
-}).then(function(response){
+}).then(function (response) {
 
-console.log(response);
+    console.log(response);
 
-}) 
+})
+let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+$.ajax({
+    url: weatherUrl,
+    method: "GET"
+}).then(function (response) {
+
+    console.log(response);
+
+})
+let lat = "41.85";
+let lon = "-87.65";
+
+let uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+$.ajax({
+    url: uvIndexUrl,
+    method: "GET"
+}).then(function (response) {
+
+    console.log(response);
+
+})
+
+
+// $.ajax({
+//     url: "w/e api you are requesting",
+//     method: "GET"
+//    }).then(function(Resdata){
+//     // [ of objects ]
+//        // let Resdata = [{} sent from api]
+//     console.log(Resdata);
+//     buildCardDiv(Resdata)
+//    })
+//    function buildCard(data) {
+//        let cardEle = $("<div class='card'");
+//        // PSEUDO CODE: let h1Ele = $("tag" + data.name + "endTag");
+//        // append to the card div 5 day forecast
+//    }
+//    // let data = Resdata;
+//    function buildCardDiv(data) {
+//        for (let i = 0; i < data.length; i++) {
+//            const element = data[i];
+//            element.tempF;
+//            buildCard(element);
+//        }  
+//    }
+//    buildCardDiv();
+
+
+
+
+
