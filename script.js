9ba0537dc84736c81a51e23d6f8d44c6f27c7d8a
Joe Brimeyer <jbrimeyer@gmail.com>
2020-05-01 22:53:29 -0700

    added functions for the ajax call and some HTML styling.

--- script.js
+++ script.js
@@ -9,47 +9,58 @@
 //  ..................
 //  uv
 //  
-let searchCity = "";
-let cityName = "Chicago";
-let apiKey = "f61c25ccc3ebc66abfbc574449b8e000";
-let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-$.ajax({
-    url: weatherUrl,
-    method: "GET"
-}).then(function (response) {
 
-    console.log(response);
 
-})
-let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
-$.ajax({
-    url: weatherUrl,
-    method: "GET"
-}).then(function (response) {
-
-    console.log(response);
-
-})
+// let searchResult = $("");
 let lat = "41.85";
 let lon = "-87.65";
+let cityName = "Chicago";
+let apiKey = "f61c25ccc3ebc66abfbc574449b8e000";
+// let searchResult = "";
 
-let uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
-$.ajax({
-    url: uvIndexUrl,
-    method: "GET"
-}).then(function (response) {
+// let searchBtn = $("");
+function getWeatherByCity() {
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    $.ajax({
+        url: weatherUrl,
+        method: "GET"
+    }).then(function (response) {
 
-    console.log(response);
+        console.log(response);
 
-})
+    })
+}
+function getWeatherFiveDay() {
+    let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    $.ajax({
+        url: fiveDayUrl,
+        method: "GET"
+    }).then(function (response) {
 
+        console.log(response);
+
+    })
+}
+function getWeatherByLatLon() {
+    let uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    $.ajax({
+        url: uvIndexUrl,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response);
+
+    })
+}
 $("#search-button").on("click", function (event) {
     event.preventDefault();
+    let searchResult = $("#search-city").val().trim();
+    console.log(searchResult);
 
-    let buttonID = $(this).attr("id");
-    console.log(buttonID);
 });
+// }
 
+// }
 // $.ajax({
 //     url: "w/e api you are requesting",
 //     method: "GET"
