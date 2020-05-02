feb47f6863f4b5f9f65ec9cf9131d11a7c82cbe9
Joe Brimeyer <jbrimeyer@gmail.com>
2020-05-02 03:30:34 -0700

    organized functions based on event handler.

--- script.js
+++ script.js
@@ -14,21 +14,22 @@
 
 // let searchResult = $("");
 let apiKey = "f61c25ccc3ebc66abfbc574449b8e000";
-let cityName = "Chicago";
+let cityName;
 // let searchResult;
 let lat = "41.85";
 let lon = "-87.65";
-
+let searchCity;
 // let searchBtn = $("");
 function getWeatherByCity() {
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`;
     $.ajax({
         url: weatherUrl,
         method: "GET"
     }).then(function (response) {
+       
 
         console.log(response);
-        getWeatherByLatLon();
+        getWeatherByFiveDay();
 
     })
 }
@@ -40,6 +41,7 @@ function getWeatherFiveDay() {
     }).then(function (response) {
 
         console.log(response);
+        getWeatherByLatLon()
 
     })
 }
@@ -51,7 +53,7 @@ function getWeatherByLatLon() {
     }).then(function (response) {
 
         console.log(response);
-        getWeatherFiveDay()
+        
 
     })
 }
@@ -60,11 +62,13 @@ $("#search-button").on("click", function (event) {
     let searchCity = $("#search-city").val().trim();
     // searchResult = ("#search-city");
     console.log(searchCity);
+    // searchCity = cityName;
+    getWeatherByCity()
 
     
 
 });
-getWeatherByCity()
+
 // }
 
 // }
