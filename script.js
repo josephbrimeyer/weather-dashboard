6c81bc16d52c35b3b9d55eb42577591ca9c3e250
Joe Brimeyer <jbrimeyer@gmail.com>
2020-05-02 20:22:54 -0700

    populated first card with jQuery.

--- script.js
+++ script.js
@@ -1,77 +1,69 @@
-//  ......id's to declare with jQuery....
-//  city, date, icon
-//  temperature
-//  humidity
-//  wind speed
-//  uv
-//  ................
-//  need to get and set the values to a variable for lat and lon from the object of getWeatherByCity and concatinate to the function of getWeatherByLatLon.
-//  lon
-//  lat
-//  set a new variable for lat and lon.
-//  ..................
 
-
-// let searchResult = $("");
+//  declare the variables...
 let apiKey = "f61c25ccc3ebc66abfbc574449b8e000";
 let cityName;
-// let searchResult;
-let lat = "41.85";
-let lon = "-87.65";
-let searchCity;
-// let searchBtn = $("");
+let searchCity = "";
+// set the event listener...
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    searchCity = $("#search-city").val().trim();
+    console.log(searchCity);
+    getWeatherByCity()
+});
+//  this function 
 function getWeatherByCity() {
     let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}`;
     $.ajax({
         url: weatherUrl,
         method: "GET"
     }).then(function (response) {
-       
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+        let cityName = searchCity + "";
+        let temperature = response.main.temp;
+        let humidity = response.main.humidity;
+        let windSpeed = response.wind.speed;
+        let icon = response.weather[0].icon;
+        let iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
+        $("#temperature").text(temperature);
+        $("#humidity").text(humidity);
+        $("#windspeed").text(windSpeed);
+        $("#city").text(cityName);
+        let date = new Date().toLocaleString();
+        $("#date").text(date);
+        $("#img").attr("src", iconUrl);
+        console.log(response);
+        let queryLat = lat;
+    let queryLon = lon;
+    let uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${queryLat}&lon=${queryLon}`;
+    $.ajax({
+        url: uvIndexUrl,
+        method: "GET"
+    }).then(function (response2) {
+        let uvValue = response2.value;
+        $("#uv").text(uvValue);
 
         console.log(response);
-        getWeatherByFiveDay();
-
+        getWeatherFiveDay();
+    })
     })
 }
+function getWeatherByLatLon() {
+    
+}
 function getWeatherFiveDay() {
-    let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}`;
     $.ajax({
         url: fiveDayUrl,
         method: "GET"
     }).then(function (response) {
 
         console.log(response);
-        getWeatherByLatLon()
 
     })
 }
-function getWeatherByLatLon() {
-    let uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
-    $.ajax({
-        url: uvIndexUrl,
-        method: "GET"
-    }).then(function (response) {
 
-        console.log(response);
-        
-
-    })
-}
-$("#search-button").on("click", function (event) {
-    event.preventDefault();
-    let searchCity = $("#search-city").val().trim();
-    // searchResult = ("#search-city");
-    console.log(searchCity);
-    // searchCity = cityName;
-    getWeatherByCity()
-
-    
-
-});
-
-// }
-
-// }
+// ..................................................................................................
 // $.ajax({
 //     url: "w/e api you are requesting",
 //     method: "GET"
